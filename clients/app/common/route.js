46ab0c4ef6923e936d072e0ae123d5522a592d6f
Anthony Nourian <annourian@gmail.com>
2016-04-08 19:33:29 -0700

    (feat) Route Commit Fix #8 Fix #4 Fix #1

---
+++ clients/app/common/route.js
@@ -0,0 +1,25 @@
+angular.module('dogSurfing')
+  .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/')
+    $stateProvider
+      .state('home', {
+        url:'/',
+        templateUrl: 'app/components/main/main.html',
+        controller: 'mainController' 
+      })
+      .state('listings', {
+        url:'/listings',
+        templateUrl: 'app/components/listings/listings.html',
+        controller: 'listingsController'
+      })
+      .state('profile', {
+        url:'/profile',
+        templateUrl: 'app/components/profile/profile.html',
+        controller: 'profileController'
+      })
+      .state('create', {
+        url:'/create',
+        templateUrl: 'app/components/create/create.html',
+        controller: 'createController'
+      })
+  }])
