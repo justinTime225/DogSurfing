9a99d269957a11fc58883f461b9f70ccd11c8fb3
Anthony Nourian <annourian@gmail.com>
2016-04-11 10:34:00 -0700

    (feat) Add getData feature Add listing template/data update dataFactory

--- clients/app/components/listings/listings.controller.js
+++ clients/app/components/listings/listings.controller.js
@@ -1,3 +1,10 @@
 angular.module('dogSurfing')
-  .controller('listingsController', function ($scope){
+  .controller('listingsController', function ($scope, dataFactory){
+    $scope.getData = function(){
+      dataFactory.getListings().then(function(data){
+        $scope.listings = data;
+      });
+    };
+    $scope.getData();
+
   });
