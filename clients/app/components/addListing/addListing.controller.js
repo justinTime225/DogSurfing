2e1c437ace63590f77e04dade81fdce92479c49b
Justin <justinminh225@gmail.com>
2016-04-11 09:50:30 -0700

    (feat) create addListing controller

--- clients/app/components/addListing/addListing.controller.js
+++ clients/app/components/addListing/addListing.controller.js
@@ -1,4 +1,10 @@
 angular.module('dogSurfing')
-.controller('addListingController', function ($scope){
-  
-})
+.controller('addListingController', function ($scope, dataFactory){
+  $scope.createPost = function(name, email, message) {
+    dataFactory.createPost({name: name, email: email, message: message})
+    .then(function(res) {
+      console.log(res);
+    });
+  };
+});
+
